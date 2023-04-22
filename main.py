fdbd9fdcae7db08435433a93fe3a9afcc6b72299
Dr. AInme <109267676+HappyOracle@users.noreply.github.com>
2023-04-22 11:54:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import filecmp, os
 
 
 
-api_id = 17876697
-api_hash = 'f57224fa791de500d30a17a4fbb650ec'
+api_id = 
+api_hash = 
 client = TelegramClient('anon', api_id, api_hash)
-chat = 418820330  # 'me', 5103866250, 1491627223, 418820330, -741750752(test), -1001299652068(it)
+chat = 'me'
 client.start()
 
 connGG = pyodbc.connect('Driver={SQL SERVER};'
